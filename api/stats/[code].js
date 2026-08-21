4d7fe2668452ebe9ad7e7dd66649bffac9816e34
raphael AUDONNET <raphafoot42@gmail.com>
2026-08-21 10:33:23 +0200

    Fix formatting of ADMIN_API_KEY declaration

--- api/stats/[code].js
+++ api/stats/[code].js
@@ -4,7 +4,7 @@
 
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_KEY;
-const ADMIN_API_KEY = process.env.ADMIN_API_KEY || "";
+const ADMIN_API_KEY = process.env.ADMIN_API_KEY || ""; 
 
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
